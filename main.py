a971d4d3922a118c3b1e52344ea59e950a6856f7
kim.wang1978@gmail.com <kim.wang1978@gmail.com>
2024-06-04 10:06:00 +0900

    2024-06-04 10:06

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ urls = [
     'https://m3u.ibert.me/txt/j_home.txt',
     'https://raw.githubusercontent.com/gaotianliuyun/gao/master/list.txt',
     'https://gitee.com/xxy002/zhiboyuan/raw/master/zby.txt',
-    'https://raw.githubusercontent.com/mlvjfchen/TV/main/iptv_list.txt',
+    'https://raw.githubusercontent.com/mlvjfchen/TV/main/iptv_list.txt', #每天早晚各自动更新1次 2024-06-03 17:50
     'https://raw.githubusercontent.com/fenxp/iptv/main/live/ipv6.txt',  #1小时自动更新1次11:11 2024/05/13
     'https://raw.githubusercontent.com/fenxp/iptv/main/live/tvlive.txt', #1小时自动更新1次11:11 2024/05/13
     'https://gitlab.com/p2v5/wangtv/-/raw/main/lunbo.txt'
