49edbafe6e58779e37777e59be048260aac28779
kim.wang1978@gmail.com <kim.wang1978@gmail.com>
2024-08-24 22:00:53 +0900

    Update 2024-08-24 22:00

--- blacklist/blacklist.py
+++ blacklist/blacklist.py
@@ -271,29 +271,30 @@ if __name__ == "__main__":
         'https://gitlab.com/p2v5/wangtv/-/raw/main/wang-tvlive.txt',
         'https://raw.githubusercontent.com/kimwang1978/tvbox/main/%E5%A4%A9%E5%A4%A9%E5%BC%80%E5%BF%83/lives/%E2%91%AD%E5%BC%80%E5%BF%83%E7%BA%BF%E8%B7%AF.txt',
         'https://raw.githubusercontent.com/mlvjfchen/TV/main/iptv_list.txt', 
-        'https://raw.githubusercontent.com/gdstchdr1/IPTV/main/bc.txt',  #ADD 【2024-08-01 10:40:29】
-        'https://raw.githubusercontent.com/skddyj/iptv/main/IPTV.m3u',  #ADD 【2024-08-01 10:40:29】
-        'https://raw.githubusercontent.com/wwb521/live/main/tv.m3u',  #ADD 【2024-08-01 10:40:29】
-        'https://raw.githubusercontent.com/lalifeier/IPTV/main/txt/IPTV.txt',  #ADD 【2024-08-01 10:40:29】
-        'https://raw.githubusercontent.com/yoursmile66/TVBox/main/live.txt',  #ADD 【2024-08-01 10:40:29】
-        'https://raw.githubusercontent.com/hujingguang/ChinaIPTV/main/cnTV_AutoUpdate.m3u8', #15分钟更新1次   #ADD 【2024-08-01 10:40:29】
-        'https://raw.githubusercontent.com/PizazzGY/TVBox/main/live.txt', #ADD 【2024-08-01 10:40:29】
-        'https://raw.githubusercontent.com/pxiptv/live/main/iptv.txt', #ADD 【2024-08-02 16:48:40】#每日更新1次
-        'https://notabug.org/vnjd/yydu/raw/master/yyfug.txt', #ADD 【2024-08-06】
-        'https://tvkj.top/tvlive.txt', #ADD 【2024-08-06】
-        'https://pan.beecld.com/f/OXMcA/%E6%98%A5%E8%B5%A2%E5%A4%A9%E4%B8%8B.txt', #ADD 【2024-08-06】
-        'http://kxrj.site:55/lib/kx2024.txt',   #ADD 【2024-08-07】
-        'https://raw.githubusercontent.com/yuanzl77/IPTV/main/live.txt',   #ADD 2024-08-05 每天更新一次，量太多转到blacklist处理
-        'https://raw.githubusercontent.com/balala2oo8/iptv/main/o.m3u',   #ADD 【2024-08-07】#每日更新2次
-        'https://wzsvip.github.io/ipv4.txt',   #ADD 【2024-08-08】
-        'http://wz.42web.io/ipv4.txt',   #ADD 【2024-08-08】
-        'https://wzsvip.github.io/ipv4.m3u',   #ADD 【2024-08-08】
-        #'http://ttkx.live:55/lib/kx2024.txt',   #ADD 【2024-08-10】每日更新3次，移动到main.py
-        #'http://mywlkj.ddns.net:5212/f/EErCL/%E5%8F%B0%E6%B9%BE%E7%94%B5%E8%A7%86TV.txt',   #ADD 【2024-08-10】
-        'http://yuhuahx.com/dsj66.txt',   #ADD 【2024-08-14】
-        'https://raw.gitcode.com/xiaoqi719/yingshi/raw/main/zhibo.txt',  #ADD 【2024-08-20】
-        #'http://xhztv.top/tvlive.txt',  #ADD 【2024-08-20】
-        'http://gg.gg/cctvgg'   #ADD 【2024-08-10】
+        'https://raw.githubusercontent.com/gdstchdr1/IPTV/main/bc.txt',  # 【2024-08-01 10:40:29】
+        'https://raw.githubusercontent.com/skddyj/iptv/main/IPTV.m3u',  # 【2024-08-01 10:40:29】
+        'https://raw.githubusercontent.com/wwb521/live/main/tv.m3u',  # 【2024-08-01 10:40:29】
+        'https://raw.githubusercontent.com/lalifeier/IPTV/main/txt/IPTV.txt',  # 【2024-08-01 10:40:29】
+        'https://raw.githubusercontent.com/yoursmile66/TVBox/main/live.txt',  #【2024-08-01 10:40:29】
+        'https://raw.githubusercontent.com/hujingguang/ChinaIPTV/main/cnTV_AutoUpdate.m3u8', #15分钟更新1次   # 【2024-08-01 10:40:29】
+        'https://raw.githubusercontent.com/PizazzGY/TVBox/main/live.txt', # 【2024-08-01 10:40:29】
+        'https://raw.githubusercontent.com/pxiptv/live/main/iptv.txt', # 【2024-08-02 16:48:40】#每日更新1次
+        'https://notabug.org/vnjd/yydu/raw/master/yyfug.txt', # 【2024-08-06】
+        'https://tvkj.top/tvlive.txt', # 【2024-08-06】
+        'https://pan.beecld.com/f/OXMcA/%E6%98%A5%E8%B5%A2%E5%A4%A9%E4%B8%8B.txt', # 【2024-08-06】
+        'http://kxrj.site:55/lib/kx2024.txt',   # 【2024-08-07】
+        'https://raw.githubusercontent.com/yuanzl77/IPTV/main/live.txt',   # 2024-08-05 每天更新一次，量太多转到blacklist处理
+        'https://raw.githubusercontent.com/balala2oo8/iptv/main/o.m3u',   # 【2024-08-07】#每日更新2次
+        'https://wzsvip.github.io/ipv4.txt',   # 【2024-08-08】
+        'http://wz.42web.io/ipv4.txt',   # 【2024-08-08】
+        'https://wzsvip.github.io/ipv4.m3u',   # 【2024-08-08】
+        #'http://ttkx.live:55/lib/kx2024.txt',   # 【2024-08-10】每日更新3次，移动到main.py
+        #'http://mywlkj.ddns.net:5212/f/EErCL/%E5%8F%B0%E6%B9%BE%E7%94%B5%E8%A7%86TV.txt',   # 【2024-08-10】
+        'http://yuhuahx.com/dsj66.txt',   # 【2024-08-14】
+        'https://raw.gitcode.com/xiaoqi719/yingshi/raw/main/zhibo.txt',  # 【2024-08-20】
+        #'http://xhztv.top/tvlive.txt',  # 【2024-08-20】
+        'https://raw.githubusercontent.com/Andreayoo/ming/main/IPTV.txt', #【2024-08-24】
+        'http://gg.gg/cctvgg'   # 【2024-08-10】
         #'',
         #''
     ]
